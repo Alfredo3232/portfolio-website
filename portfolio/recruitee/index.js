d725c8cfa1d4b998b2e6968c862b61568c897655
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-07-13 09:30:00 -0700

    updating website

---
+++ portfolio/recruitee/index.js
@@ -0,0 +1,263 @@
+// THIS IS AN EXAMPLE ON HOW TO DO A TIMELINE
+
+// <!DOCTYPE html>
+// <html prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+
+// <head>
+//   <meta charset="utf-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+//   <title>Recruitee - Alfredo Rojas - full-stack freelance software engineer</title>
+//   <meta name="viewport" content="width=device-width">
+
+//   <!--[if lt IE 9]>
+//       <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+//     <![endif]-->
+
+//   <link rel="stylesheet" href="/css/main.dda5b8ea.css">
+//   <script type="text/javascript" src="/scripts/main.10647a82.js"></script>
+
+
+
+
+
+//   <meta name="description"
+//     content="A beautiful online tool to help you find, screen and hire the best talent. Available on Android & iOS.">
+//   <link rel="canonical" href="https://jtom.me/portfolio/recruitee/" />
+
+
+
+//   <meta property="og:title" content="Recruitee">
+//   <meta property="og:type" content="website">
+//   <meta property="og:site_name" content="Alfredo Rojas - full-stack freelance software engineer">
+//   <meta property="og:locale" content="en_US">
+//   <meta property="og:url" content="https://jtom.me/portfolio/recruitee/">
+//   <meta property="og:description"
+//     content="A beautiful online tool to help you find, screen and hire the best talent. Available on Android & iOS.">
+//   <meta property="og:image" content="https://jtom.me//images/portfolio/recruitee/7.png">
+//   <!-- <meta property="og:image:type" content="image/png"> -->
+//   <!-- <meta property="og:image:width" content="152"> -->
+//   <!-- <meta property="og:image:height" content="152"> -->
+
+
+//   <meta property="fb:profile_id" content="AlfredoRojas">
+
+//   <meta property="fb:admins" content="AlfredoRojas">
+// </head>
+
+// <body class="parent-portfolio">
+//   <aside class="site-sidebar">
+//     <div class="site-sidebar-inner h-card">
+//       <a href="/" rel="me" class="person u-url">
+//         <div class="person-avatar u-photo">
+//           <img src="/images/self.jpg" alt="" class="avatar">
+//         </div>
+//         <div class="person-content">
+//           <h1 class="person-title p-name">Alfredo Rojas</h1>
+//           <h2 class="person-subtitle p-job-title p-note">Full-stack Software Engineer</h2>
+//         </div>
+//       </a>
+
+//       <nav class="block main-navigation">
+//         <div class="navigation-extend-bar">
+//           <div class="social-icons">
+//             <a rel="me" href="/cdn-cgi/l/email-protection#2842494b4d4368425c474506454d"
+//               class="button button-icon u-email" title="mail: Alfredo2001@me.com">
+//               <i class="fa fa-envelope"></i>
+//             </a>
+
+//             <a rel="me" href="www.linkedin.com/in/AlfredoRojas1" class="button button-icon"
+//               title="linkedin: AlfredoRojas1">
+//               <i class="fa fa-linkedin-square"></i>
+//             </a>
+
+//             <a rel="me" href="https://github.com/arojas32" class="button button-icon" title="github: AlfredoRojas">
+//               <i class="fa fa-github-square"></i>
+//             </a>
+// <!-- 
+//             <a rel="me" href="https://facebook.com/AlfredoRojas" class="button button-icon"
+//               title="facebook: AlfredoRojas">
+//               <i class="fa fa-facebook-square"></i>
+//             </a>
+
+//             <a rel="me" href="https://twitter.com/AlfredoRojas1" class="button button-icon"
+//               title="twitter: @AlfredoRojas1">
+//               <i class="fa fa-twitter-square"></i>
+//             </a> -->
+//           </div>
+
+//           <a href="#" class="navigation-extend-button js-extend-main-navigation">
+//             <i class="fa fa-bars"></i>
+//           </a>
+//         </div>
+
+//         <div class="navigation-extendable">
+//           <ul>
+//             <li class=""><a href="/skills/">Skills</a></li>
+//             <li class="current"><a href="/portfolio/">Portfolio</a></li>
+//             <li class=""><a href="/contact/">Contact me</a></li>
+//           </ul>
+//           <ul>
+//             <!-- <li class=""><a href="/about-me/">About me</a></li> -->
+
+//             <li class=""><a href="/cv/">CV</a></li>
+//             <li class=""><a href="/talks/">Presentations</a></li>
+
+//           </ul>
+//         </div>
+//       </nav>
+
+//       <div class="block block-social">
+//         <div class="block-title">Get in touch</div>
+//         <div class="block-content">
+//           <div class="social-icons">
+//             <a rel="me" href="/cdn-cgi/l/email-protection#1a707b797f715a706e757734777f"
+//               class="button button-icon u-email" title="mail: Alfredo2001@me.com">
+//               <i class="fa fa-envelope"></i>
+//             </a>
+
+//             <a rel="me" href="www.linkedin.com/in/AlfredoRojas1" class="button button-icon"
+//               title="linkedin: AlfredoRojas1">
+//               <i class="fa fa-linkedin-square"></i>
+//             </a>
+
+//             <a rel="me" href="https://github.com/arojas32" class="button button-icon" title="github: AlfredoRojas">
+//               <i class="fa fa-github-square"></i>
+//             </a>
+// <!-- 
+//             <a rel="me" href="https://facebook.com/AlfredoRojas" class="button button-icon"
+//               title="facebook: AlfredoRojas">
+//               <i class="fa fa-facebook-square"></i>
+//             </a>
+
+//             <a rel="me" href="https://twitter.com/AlfredoRojas1" class="button button-icon"
+//               title="twitter: @AlfredoRojas1">
+//               <i class="fa fa-twitter-square"></i>
+//             </a> -->
+//           </div>
+//         </div>
+//       </div>
+
+
+
+//       </div>
+//     </div>
+//   </aside>
+
+//   <div class="site-main">
+//     <div class="site-main-inner">
+//       <div class="main-section project-section">
+//         <h1 class="main-title">Recruitee</h1>
+//         <h2 class="project-subtitle">A beautiful online tool to help you find, screen and hire the best talent</h2>
+
+//         <div class="project-descriptions">
+
+//           <div class="project-description project-general_description">
+//             <p>The mobile application for Android &amp; iOS includes almost all of the Recruitee’s web panel
+//               functionalities, like:</p>
+
+//             <ul>
+//               <li><strong>Candidate details</strong> - all candidate details and activities in one handy overview.</li>
+//               <li><strong>Team communication</strong> - leave internal notes for teammates or directly contact
+//                 candidates.</li>
+//               <li><strong>Smart overview</strong> - A clear overview of all candidates and recruitment stages.</li>
+//               <li><strong>Powerful search</strong> - search for candidate details, resumes or test scores in the App.
+//               </li>
+//             </ul>
+
+//             <p>The app has been available on mobile appstores for both major platforms: Android and iOS.</p>
+
+//             <p>Design and UI sketches were prepared beforehand by the Recruitee’s team.</p>
+
+//           </div>
+
+
+
+//           <div class="project-description project-tech_description">
+
+//             <h3>Tech wrap-up</h3>
+
+//             <ul>
+//               <li>a hybrid mobile application (for Android &amp; iOS)</li>
+//               <li>done in AngularJS and Ionic Framework</li>
+//               <li>deployable to mobile stores as native app via Apache Cordova</li>
+//               <li>tools used during development: Gulp, Jade, SASS, CoffeeScript</li>
+//             </ul>
+
+
+
+
+//             <h5 class="technologies-title">Technologies used</h5>
+
+//             <ul class="tech-tags">
+
+//               <li><a href='https://angular.io/' rel='nofollow' target='_blank'>Angular</a></li>
+
+//               <li><a href="https://ionicframework.com/" rel='nofollow' target='_blank'>Ionic Framework</a></li>
+
+//               <li><a href="https://cordova.apache.org/" rel='nofollow' target='_blank'>Apache Cordova</a></li>
+
+//             </ul>
+
+//           </div>
+
+//         </div>
+
+
+//         <a href="https://recruitee.com/" class="button button-red">
+//           <i class="fa fa-external-link"></i>
+//           View online
+//         </a>
+
+
+
+//         <div class="project-images">
+
+//           <img src="/images/portfolio/recruitee/11.7dbe32d0.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/12.6a267e2f.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/1.9bf5e4b4.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/2.de1b1eb2.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/3.aff15a7a.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/4.495031bc.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/5.3957185a.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/6.a545b762.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/7.6a019244.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/8.04f9475c.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/9.6b103bb7.png" alt="Recruitee">
+
+//           <img src="/images/portfolio/recruitee/10.b0c57230.png" alt="Recruitee">
+
+//         </div>
+
+//       </div>
+
+//     </div>
+//   </div>
+
+
+
+//   <!-- Disqus: comments count -->
+//   <script type="text/javascript">
+//     var disqus_shortname = "jtom";
+
+//     /* * * DON'T EDIT BELOW THIS LINE * * */
+//     (function () {
+//       var s = document.createElement('script'); s.async = true;
+//       s.type = 'text/javascript';
+//       s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+//       (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+//     }());
+//   </script>
+
+// </body>
+
+// </html>
